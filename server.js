ee1a28c5c7477a5f05a6da696d6e02222bbaf58d
Doug Sebastian <dasebast@gmail.com>
2015-02-17 22:40:01 -0700

    Passport partially implemented

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// REQUIRE ===============================
+// ============================ REQUIRE ===============================
 var Express = require('express');
 var App = Express();
 var Session = require('express-session');
@@ -7,18 +7,38 @@ var Mongoose = require('mongoose');
 var Passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-// CONTROLLERS ===========================
+// ============================ CONTROLLERS ===========================
 
 
 
 
-// MIDDLEWARE ============================
+// ============================ MIDDLEWARE ============================
+	// +++ Passport -- Configure +++
+Passport.use(new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password'
+}, function(username, password, done) {     // How all does this work?
+	User.findOne({email: username}).exec().then(function(user) {
+		if(!user) {
+			return done(null, false, {message: 'Incorrect username.'});
+		}
+		user.comparePassword(password).then(function(isMatch) {
+			if(!isMatch) {
+				return done(null, false, {message: 'Incorrect password.'});
+			}
+			return done(null, user);
+		})
+	});
+}));
 
+Passport.serializeUser(function(user, done) {
+	done(null, user);
+});
 
-
-
-
-
+Passport.deserializeUser(function(user, done) {
+	done(null, obj);
+});
+	// +++ END Passport -- Configure +++
 
 
 App.use(Express.static(__dirname + '/public'));
@@ -27,11 +47,32 @@ App.use(Session({ secret: 'supersecretcode'}));
 App.use(Passport.initialize());
 App.use(Passport.session());
 
-// AUTHENTICATION ========================
+
+// ============================ AUTHENTICATION ========================
+App.post('/api/auth', Passport.authenticate('local'), function(req, res) {
+	return res.status(200).end();
+});
+
+App.post('/api/register', function(req, res) {
+	var newUser = new User(req.body);
+	newUser.save(function(err, user) {
+		if(err) {
+			return res.status(500).end();
+		}
+		return res.json(user);
+	});
+});
+
+var isAuthed = function(req, res, next) {
+	if(!req.isAuthenticated()) {
+		return res.status(403).end();
+	}
+	return next();
+};
 
 
 
-// ENDPOINTS =============================
+// ============================ ENDPOINTS =============================
 
 
 
